bb6058641345de1a9c7740adebf7da5c75707950
Jason Raimondi <jason@raimondi.us>
2019-09-26 22:27:46 -0700

    Clean up.

--- src/modules/user/register_resolver.ts
+++ src/modules/user/register_resolver.ts
@@ -1,27 +1,28 @@
-import { Arg, Mutation, Query, Resolver } from "type-graphql";
-import { hash } from "bcryptjs";
-import { v4 } from "uuid";
+import {Arg, Mutation, Query, Resolver} from "type-graphql";
+import {hash} from "bcryptjs";
+import {v4} from "uuid";
 
-import { RegisterInput } from "@modules/user/register/register_input";
-import { User } from "@entity/user";
+import {RegisterInput} from "@modules/user/register/register_input";
+import {User} from "@entity/user";
 
 @Resolver(User)
 export class RegisterResolver {
     @Query(() => User)
-    async user(
-        @Arg('uuid') uuid: string,
-    ) {
-        return await User.findOne({ uuid });
+    async user(@Arg('uuid') uuid: string) {
+        return await User.findOne({uuid});
     }
 
     @Mutation(() => User)
     async register(
-        @Arg("data") data: RegisterInput,
+        @Arg("data") {firstName, lastName, email, password}: RegisterInput,
     ): Promise<User> {
-        const hashedPassword = await hash(data.password, 12);
+        const hashedPassword = await hash(password, 12);
+        const uuid = v4();
         const user = await User.create({
-            ...data,
-            uuid: v4(),
+            uuid,
+            firstName,
+            lastName,
+            email,
             password: hashedPassword,
         });
         await user.save();
