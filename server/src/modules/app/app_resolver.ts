1c83c3979ffb30d9e7e96ab5e45c3b0c591a6a51
Jason Raimondi <jason@raimondi.us>
2019-10-14 20:18:46 -0700

    Wip.

--- server/src/modules/app/app_resolver.ts
+++ server/src/modules/app/app_resolver.ts
@@ -1,8 +1,10 @@
+import { injectable } from "inversify";
 import { Query, Resolver } from "type-graphql";
 
 import { version } from "@root/package.json";
 
 @Resolver()
+@injectable()
 export class AppResolver {
     @Query(() => String!)
     version() {
