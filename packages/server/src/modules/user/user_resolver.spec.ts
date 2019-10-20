a1835550baa376988392716150703de4e5bd49b3
Jason Raimondi <jason@raimondi.us>
2019-10-20 12:54:04 -0700

    Add test coverage to user_resolver.

---
+++ packages/server/src/modules/user/user_resolver.spec.ts
@@ -0,0 +1,52 @@
+import { TestingInversifyContainer } from "@/lib/testing_inversify_container";
+import { User } from "@/entity/user_entity";
+import { Role } from "@/entity/role_entity";
+import { ForgotPassword } from "@/entity/forgot_password_entity";
+import { Permission } from "@/entity/permission_entity";
+import { EmailConfirmation } from "@/entity/email_confirmation_entity";
+import { UserResolver } from "@/modules/user/user_resolver";
+import { UserRepository } from "@/lib/repository/user/user_repository";
+import { REPOSITORY } from "@/lib/constants/inversify";
+
+describe("register_resolver", () => {
+    const entities = [User, Role, Permission, ForgotPassword, EmailConfirmation];
+
+    let container: TestingInversifyContainer;
+    let userRepository: UserRepository;
+
+    beforeEach(async () => {
+        container = await TestingInversifyContainer.create(entities);
+        userRepository = container.get<UserRepository>(REPOSITORY.UserRepository);
+    });
+
+    describe("users function", () => {
+        test("user is registered with email confirmation", async () => {
+            // arrange
+            const resolver = container.get<UserResolver>(UserResolver);
+            const user = await userRepository.save(User.create({ email: "jason@raimondi.us" }));
+
+            // act
+            const result = await resolver.user(user.uuid);
+
+            // assert
+            expect(result.uuid).toBe(user.uuid);
+            expect(result.email).toBe(user.email);
+            expect(result.firstName).toBe(user.firstName);
+        });
+    });
+
+    describe("users function", () => {
+        test("user is registered with email confirmation", async () => {
+            // arrange
+            const resolver = container.get<UserResolver>(UserResolver);
+            await userRepository.save(User.create({ email: "jason@raimondi.us" }));
+            await userRepository.save(User.create({ email: "jason1@raimondi.us" }));
+
+            // act
+            const result = await resolver.users();
+
+            // assert
+            expect(result.length).toBe(2);
+        });
+    });
+});
