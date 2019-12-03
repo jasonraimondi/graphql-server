4a6a05431d068b368e2a62b01d44434c50b39f64
Jason Raimondi <jason@raimondi.us>
2019-12-02 22:48:53 -0800

    Add generate graphql schema bin script. Add lerna dev script from root.

---
+++ packages/graphql-api/bin/generate_graphql_schema.ts
@@ -0,0 +1,23 @@
+import "reflect-metadata";
+import "source-map-support/register";
+import "tsconfig-paths/register";
+
+import { buildSchema } from "type-graphql";
+import { join } from "path";
+
+const projectRoot = join(__dirname, "../../../");
+const resolvers = join(projectRoot, "packages/graphql-api/src/modules/**/*_resolver.ts");
+const outputSchema = join(projectRoot, "packages/web/.schema.graphql");
+
+console.log("source: ", resolvers);
+console.log("output: ", outputSchema);
+
+(async () => {
+  await buildSchema({
+    resolvers: [resolvers],
+    emitSchemaFile: {
+      path: outputSchema,
+      commentDescriptions: true,
+    },
+  });
+})();
