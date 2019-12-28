916ebab64d3f556cecfe1bcb1d557c381484fd64
Jason Raimondi <jason@raimondi.us>
2019-12-27 19:11:51 -0700

    Set token refresh timeouts to an appropriate value.

--- packages/api/src/lib/constants/config.ts
+++ packages/api/src/lib/constants/config.ts
@@ -1,6 +1,6 @@
 export const ENV = {
-  accessTokenTimeout: "10s",
-  refreshTokenTimeout: "1m",
+  accessTokenTimeout: "15m",
+  refreshTokenTimeout: "7d",
   accessTokenSecret: process.env.ACCESS_TOKEN_SECRET ?? "",
   refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET ?? "",
   enableDebugging: !!process.env.ENABLE_DEBUGGING,
