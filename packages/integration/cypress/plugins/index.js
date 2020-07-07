4dfd1e2881f49418630d82f1cb6ed37a92cbfb92
Jason Raimondi <jason@raimondi.us>
2020-07-06 22:07:25 -0700

    Bump cypress to 4.9.0
    Remove cypress ts preprocessors since ts is supported w/o now

--- packages/integration/cypress/plugins/index.js
+++ packages/integration/cypress/plugins/index.js
@@ -1,9 +1,6 @@
-const cypressTypeScriptPreprocessor = require("./cy-ts-preprocessor");
-
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 module.exports = (on, config) => {
   config.env = config.env || {};
   config.env.MAILER_HTTP_URL = process.env.MAILER_HTTP_URL || "http://localhost:8025";
-  on("file:preprocessor", cypressTypeScriptPreprocessor);
   return config;
 };
